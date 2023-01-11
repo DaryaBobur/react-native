26922fdb9816f658a81d52c7d0e7fbe0eb062080
Darya <daryabobur@gmail.com>
2023-01-11 20:45:43 +0200

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,32 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, } from 'react-native';
 
+import RegistrationScreen from './Screens/RegistrationScreen'
 export default function App() {
   return (
-    <View style={styles.container}>
-    <ImageBackground style={styles.image} source={require("./assets/Photo.jpg")}>
-        {/* <Text style={styles.text}>Open up App.js to start working on your app!</Text> */}
-        <StatusBar style="auto" />
-    </ImageBackground>
-    
-
-    </View>
+  
+    <>
+    <RegistrationScreen />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0e68c',
-  },
-  text: {
-    color: "#2a2a2a",
-    fontWeight: 'bold',
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    alignItems: 'center',
-    justifyContent: "center",
-  }
-});
+
