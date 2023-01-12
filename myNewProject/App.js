b4d3b0af3c92faf86bb3f005ad93d1dd53aeb98e
Darya <daryabobur@gmail.com>
2023-01-12 21:42:15 +0200

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,9 +1,30 @@
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
+import RegistrationScreen from './Screens/RegistrationScreen';
+import { useState } from 'react';
 
-import RegistrationScreen from './Screens/RegistrationScreen'
+const loadApplication = async () => {
+  await Font.loadAsync({
+    "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
+    "Roboto-Bold": require('./assets/fonts/Roboto-Bold.ttf'),
+    "Roboto-Medium": require('./assets/fonts/Roboto-Medium.ttf')
+  })
+}
 export default function App() {
+  // const [isReady, setIsReady] = useState(false);
+  // if(!isReady) {
+  //   return (
+  //     <AppLoading 
+  //     startAsync={loadApplication}
+  //     onFinish={()=> setIsReady(true)}
+  //     onError={console.error}
+  //     />
+  //   )
+  // }
   return (
   
     <>
+  
     <RegistrationScreen />
     </>
   );
