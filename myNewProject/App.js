1dd37c7187021de645b8294d3f3590708908bd3c
Darya <daryabobur@gmail.com>
2023-02-07 17:39:38 +0200

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,14 +1,13 @@
 import React, {useState} from "react";
 import {} from "react-native";
 import * as Font from 'expo-font';
-// import { AppLoading } from 'expo';
 import AppLoading from 'expo-app-loading'
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import RegistrationScreen from './Screens/RegistrationScreen';
 import LoginScreen from './Screens/LoginScreen';
 
-const loadApplication = async () => {
+const loadFonts = async () => {
   await Font.loadAsync({
     "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
     "Roboto-Bold": require('./assets/fonts/Roboto-Bold.ttf'),
@@ -22,7 +21,7 @@ export default function App() {
   if(!isReady) {
     return (
       <AppLoading 
-      startAsync={loadApplication}
+      startAsync={loadFonts}
       onFinish={()=> setIsReady(true)}
       onError={console.error}
       />
