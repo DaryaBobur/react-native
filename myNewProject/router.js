232dc17a8756fcd18d43557ec51975d8269811b8
Darya <daryabobur@gmail.com>
2023-02-08 17:44:51 +0200

    Create router.js

---
+++ myNewProject/router.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import PostsScreen from "./Screens/mainScreen/PostsScreen";
+import CreatePostsScreen from "./Screens/mainScreen/CreatePostsScreen";
+import ProfileScreen from "./Screens/mainScreen/ProfileScreen";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+
+const AuthStack = createStackNavigator();
+const MainTab = createBottomTabNavigator();
+
+const appRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
+      <AuthStack.Navigator>
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Register"
+          component={RegistrationScreen}
+        />
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Login"
+          component={LoginScreen}
+        />
+      </AuthStack.Navigator>
+    );
+  }
+  return (
+    <MainTab.Navigator tabBarOptions={{ showLabel: false }}>
+      <MainTab.Screen
+        options={{
+          headerTitleAlign: "center",
+          tabBarIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons name="dots-grid" size={24} color="black" />
+          ),
+        }}
+        name="Posts"
+        component={PostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          headerTitleAlign: "center",
+          tabBarIcon: ({ focused, size, color }) => (
+            <Ionicons name="add" size={24} color="black" />
+          ),
+        }}
+        name="Create"
+        component={CreatePostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          headerTitleAlign: "center",
+          tabBarIcon: ({ focused, size, color }) => (
+            <Feather name="user" size={24} color="black" />
+          ),
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </MainTab.Navigator>
+  );
+};
+
+export default appRoute;
