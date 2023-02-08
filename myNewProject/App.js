ed298b0cdf79b110c73f3ca04d03c014327b374e
Darya <daryabobur@gmail.com>
2023-02-08 17:44:42 +0200

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 // import {} from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
-import { createStackNavigator } from "@react-navigation/stack";
+
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import RegistrationScreen from "./Screens/RegistrationScreen";
-import LoginScreen from "./Screens/LoginScreen";
-import PostsScreen from "./Screens/mainScreen/PostsScreen";
-import CreatePostsScreen from "./Screens/mainScreen/CreatePostsScreen";
-import ProfileScreen from "./Screens/mainScreen/ProfileScreen";
+
+
+import appRoute from "./router";
+
 
 const loadFonts = async () => {
   await Font.loadAsync({
@@ -19,11 +17,13 @@ const loadFonts = async () => {
   });
 };
 
-const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator();
+
+
+
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
+  const routing = appRoute({})
   if (!isReady) {
     return (
       <AppLoading
@@ -35,29 +35,7 @@ export default function App() {
   }
   return (
     <NavigationContainer>
-      <MainTab.Navigator>
-        <MainTab.Screen name="Posts" component={PostsScreen}/>
-        <MainTab.Screen name="CreatePosts" component={CreatePostsScreen}/>
-        <MainTab.Screen name="Profile" component={ProfileScreen}/>
-      </MainTab.Navigator>
+{routing}
     </NavigationContainer>
   );
 }
-
-
-  //    <AuthStack.Navigator> 
-  // <AuthStack.Screen
-  //         options={{
-  //           headerShown: false,
-  //         }}
-  //         name="Register"
-  //         component={RegistrationScreen}
-  //       />
-  //       <AuthStack.Screen
-  //         options={{
-  //           headerShown: false,
-  //         }}
-  //         name="Login"
-  //         component={LoginScreen}
-  //       />
-  //     </AuthStack.Navigator> 
