3623d6011af0686628460936f246470cae914c4c
Darya <daryabobur@gmail.com>
2023-02-08 15:49:43 +0200

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,37 +1,49 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {} from "react-native";
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading'
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import RegistrationScreen from './Screens/RegistrationScreen';
-import LoginScreen from './Screens/LoginScreen';
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 const loadFonts = async () => {
   await Font.loadAsync({
-    "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
-    "Roboto-Bold": require('./assets/fonts/Roboto-Bold.ttf'),
-    "Roboto-Medium": require('./assets/fonts/Roboto-Medium.ttf')
-  })
-}
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+};
 
 const AuthStack = createStackNavigator();
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  if(!isReady) {
+  if (!isReady) {
     return (
-      <AppLoading 
-      startAsync={loadFonts}
-      onFinish={()=> setIsReady(true)}
-      onError={console.error}
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setIsReady(true)}
+        onError={console.error}
       />
-    )
+    );
   }
   return (
     <NavigationContainer>
       <AuthStack.Navigator>
-        <AuthStack.Screen name="Register" component={RegistrationScreen} />
-        <AuthStack.Screen name="Login" component={LoginScreen} />
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Register"
+          component={RegistrationScreen}
+        />
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Login"
+          component={LoginScreen}
+        />
       </AuthStack.Navigator>
     </NavigationContainer>
   );
