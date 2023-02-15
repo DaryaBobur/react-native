874f99f3dcc05417a93dfa4a806dd8358ea7d312
Darya <daryabobur@gmail.com>
2023-02-15 20:05:15 +0200

    Update CreatePostsScreen.js

--- myNewProject/Screens/mainScreen/CreatePostsScreen.js
+++ myNewProject/Screens/mainScreen/CreatePostsScreen.js
@@ -1,13 +1,33 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import { View, Text, StyleSheet, Image,TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
-import { TouchableOpacity } from "react-native-gesture-handler";
+// import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CreatePostsScreen = () => {
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState(null);
+
+  const takePhoto = async() => {
+    const photo = await camera.takePictureAsync();
+    setPhoto(photo.uri);
+    console.log('photo', photo);
+  }
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera}>
-        <TouchableOpacity style={styles.snapContainer}>
+      <Camera style={styles.camera} ref={setCamera}>
+        {
+          photo && (
+            <View style={styles.photoContainer}>
+              <Image 
+              source={{uri: photo}}
+              style={{ height: 200, width: 200}}
+              />
+              
+            </View>
+          )
+        }
+        <TouchableOpacity onPress={takePhoto} style={styles.snapContainer}>
           <Text style={styles.snap}>SNAP</Text>
         </TouchableOpacity>
       </Camera>
@@ -20,9 +40,10 @@ const styles = StyleSheet.create({
   },
   camera: {
     height: 300,
-    marginTop: 32,
+    // marginBottom: 32,
+    flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-end",
   },
   snap: {
     color: "#FF6C00",
@@ -31,11 +52,18 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     borderWidth: 1,
     borderColor: "#FF6C00",
-    marginTop: 200,
+    marginBottom: 20,
     width: 50,
     height: 50,
     justifyContent: "center",
     alignItems: "center",
   },
+  photoContainer:{
+    position: 'absolute',
+    top: 50,
+    left: 20,
+    borderColor: 'red',
+    borderWidth: 1,
+  }
 });
 export default CreatePostsScreen;
