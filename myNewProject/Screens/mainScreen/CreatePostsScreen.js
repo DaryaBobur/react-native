4d7e6b707c5cdb1b5b16f03c6d1a6b8cbc426162
Darya <daryabobur@gmail.com>
2023-02-17 23:05:39 +0200

    Update CreatePostsScreen.js

--- myNewProject/Screens/mainScreen/CreatePostsScreen.js
+++ myNewProject/Screens/mainScreen/CreatePostsScreen.js
@@ -1,36 +1,42 @@
-import React, {useState} from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput  } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import { Camera } from "expo-camera";
-
-const CreatePostsScreen = ({navigation}) => {
+import { SimpleLineIcons } from "@expo/vector-icons";
+{
+  /* <SimpleLineIcons name="camera" size={24} color="black" /> */
+}
+import { AntDesign } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
+const CreatePostsScreen = ({ navigation }) => {
   const [camera, setCamera] = useState(null);
   const [photo, setPhoto] = useState(null);
 
-  const makePhoto = async() => {
+  const makePhoto = async () => {
     const photo = await camera.takePictureAsync();
     setPhoto(photo.uri);
-    console.log('photo', photo);
+    console.log("photo", photo);
   };
 
-  const sendPhoto =() => {
+  const sendPhoto = () => {
     console.log(navigation);
-    navigation.navigate('Posts', {photo})
-  }
+    navigation.navigate("Posts", { photo });
+  };
 
   return (
     <View style={styles.container}>
-
-    <Camera style={styles.camera} ref={setCamera}>
-        {
-          photo && (
-            <View style={styles.photoContainer}>
-              <Image 
-              source={{uri: photo}}
-              style={styles.photo}
-              />
-              
-            </View>
-          )} 
+      <Camera style={styles.camera} ref={setCamera}>
+        {photo && (
+          <View style={styles.photoContainer}>
+            <Image source={{ uri: photo }} style={styles.photo} />
+          </View>
+        )}
         <TouchableOpacity onPress={makePhoto} style={styles.snapContainer}>
           <Text style={styles.snap}>SNAP</Text>
         </TouchableOpacity>
@@ -39,42 +45,42 @@ const CreatePostsScreen = ({navigation}) => {
         <Text style={styles.text}>Download photo</Text>
       </View>
       <View>
-        <TextInput
-        style={styles.input}
-        placeholder="Description..."
+        <TextInput style={styles.input} placeholder="Description..." />
+      </View>
+      <View style={{}}>
+        <TextInput style={styles.input} placeholder="  Location..." />
+        <EvilIcons
+          name="location"
+          size={24}
+          style={{ position: "absolute", top: 10, left: 10, color: "#BDBDBD" }}
         />
       </View>
       <View>
-        <TextInput
-        style={styles.input}
-        placeholder="Location..."
-        />
-      </View>
-      <View>
-      <TouchableOpacity onPress={sendPhoto} style={styles.publishBtn}>
+        <TouchableOpacity onPress={sendPhoto} style={styles.publishBtn}>
           <Text style={styles.publish}>Publish post</Text>
         </TouchableOpacity>
       </View>
-    </View>
 
+      <View>
+        <TouchableOpacity style={styles.deleteBtn}>
+          <AntDesign name="delete" size={24} color="#BDBDBD" />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
-    
+    backgroundColor: "#fff",
   },
   camera: {
-    height: '50%',
+    height: 240,
     marginHorizontal: 16,
     marginTop: 32,
     alignItems: "center",
     justifyContent: "flex-end",
     marginBottom: 8,
-    // borderWidth: 1,
-    // borderColor: '#E8E8E8',
-    // borderRadius: 8,
   },
   snap: {
     color: "#FF6C00",
@@ -89,55 +95,65 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  photoContainer:{
-    position: 'absolute',
+  photoContainer: {
+    position: "absolute",
     top: 50,
     left: 100,
-    borderColor: '#FF6C00',
+    borderColor: "#FF6C00",
     borderWidth: 1,
-    borderRadius: 20
+    borderRadius: 20,
   },
   photo: {
-    height: 200, 
+    height: 200,
     width: 200,
   },
-  publishBtn:{
+  publishBtn: {
     marginHorizontal: 16,
     borderRadius: 100,
     paddingBottom: 16,
     paddingTop: 16,
     paddingRight: 118,
     paddingLeft: 118,
-    borderWidth: 1,
-    borderColor: 'transparent',
     marginBottom: 20,
-    color: 'red',
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: '#F6F6F6'
+    backgroundColor: "#F6F6F6",
   },
   publish: {
-    color: '#BDBDBD',
+    color: "#BDBDBD",
   },
   input: {
     borderBottomWidth: 1,
     borderBottomColor: "#E8E8E8",
     height: 40,
     marginHorizontal: 16,
+
     color: "#2a2a2a",
     padding: 10,
     marginBottom: 32,
     backgroundColor: "#fff",
     fontSize: 16,
   },
-  text:{
+  text: {
     fontSize: 16,
     lineHeight: 19,
-    color: '#BDBDBD',
+    color: "#BDBDBD",
   },
   textContainer: {
-    marginHorizontal: 16, 
-    marginBottom:48
-  }
+    marginHorizontal: 16,
+    marginBottom: 48,
+  },
+  deleteBtn: {
+    marginHorizontal: 153,
+    borderRadius: 20,
+    paddingBottom: 8,
+    paddingTop: 8,
+    paddingRight: 23,
+    paddingLeft: 23,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F6F6F6",
+    marginVertical: 32,
+  },
 });
 export default CreatePostsScreen;
