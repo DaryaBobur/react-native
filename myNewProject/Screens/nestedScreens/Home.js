3c8bc78d052d109df075cc676497ed657768d509
Darya <daryabobur@gmail.com>
2023-02-18 16:03:59 +0200

    Create Home.js

---
+++ myNewProject/Screens/nestedScreens/Home.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from "react";
+import { View, StyleSheet, FlatList, Image, TouchableOpacity, Button } from "react-native";
+import { Feather } from '@expo/vector-icons';
+
+
+const Home = ({route, navigation}) => {
+  const [posts, setPosts] = useState([]);
+
+useEffect(() => {
+  if (route.params) {
+    setPosts((prevState) => [...prevState, route.params]);
+  }
+}, [route.params]);
+
+console.log(posts);
+
+return (
+  <View style={styles.container}>
+    <FlatList 
+    data={posts}
+    keyExtractor={(item, index) => index.toString()}
+    renderItem={({item}) => (
+      <View style={styles.imageContainer}>
+          <Image
+          source={{uri: item.photo}}
+          style={styles.image}
+          />
+      </View>
+    )}
+    />
+    {/* <Button title="map" onPress={()=> navigation.navigate('Map')}/> */}
+<TouchableOpacity onPress={()=> navigation.navigate('Map')}>
+<Feather name="map-pin" size={24} color="red" />
+</TouchableOpacity>
+  </View>
+);
+};
+
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+},
+image: {
+  height: 240, 
+  width: 343, 
+  marginHorizontal: 16
+},
+imageContainer: {
+  marginBottom: 10,
+  justifyContent: "center",
+  alignItems: "center",
+ 
+}
+
+})
+
+export default Home;
