7ded4d5dd6c0fd39dbea01bc2b1db4bb502b1e54
Darya <daryabobur@gmail.com>
2023-02-18 16:07:25 +0200

    Update Home.js

--- myNewProject/Screens/nestedScreens/Home.js
+++ myNewProject/Screens/nestedScreens/Home.js
@@ -1,60 +1,63 @@
-import React, {useEffect, useState} from "react";
-import { View, StyleSheet, FlatList, Image, TouchableOpacity, Button } from "react-native";
-import { Feather } from '@expo/vector-icons';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
 
-
-const Home = ({route, navigation}) => {
+const Home = ({ route, navigation }) => {
   const [posts, setPosts] = useState([]);
 
-useEffect(() => {
-  if (route.params) {
-    setPosts((prevState) => [...prevState, route.params]);
-  }
-}, [route.params]);
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
 
-console.log(posts);
+  console.log(posts);
 
-return (
-  <View style={styles.container}>
-    <FlatList 
-    data={posts}
-    keyExtractor={(item, index) => index.toString()}
-    renderItem={({item}) => (
-      <View style={styles.imageContainer}>
-          <Image
-          source={{uri: item.photo}}
-          style={styles.image}
-          />
-      </View>
-    )}
-    />
-    {/* <Button title="map" onPress={()=> navigation.navigate('Map')}/> */}
-<TouchableOpacity onPress={()=> navigation.navigate('Map')}>
-<Feather name="map-pin" size={24} color="red" />
-</TouchableOpacity>
-  </View>
-);
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.imageContainer}>
+            <Image source={{ uri: item.photo }} style={styles.image} />
+          </View>
+        )}
+      />
+      <TouchableOpacity onPress={() => navigation.navigate("Map")}>
+        <Feather name="map-pin" size={24} color="#BDBDBD" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Comments")}>
+        <EvilIcons name="comment" size={24} color="#BDBDBD" />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
-
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center",
-},
-image: {
-  height: 240, 
-  width: 343, 
-  marginHorizontal: 16
-},
-imageContainer: {
-  marginBottom: 10,
-  justifyContent: "center",
-  alignItems: "center",
- 
-}
-
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    height: 240,
+    width: 343,
+    marginHorizontal: 16,
+  },
+  imageContainer: {
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Home;
