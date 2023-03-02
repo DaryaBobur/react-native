2bbcb49af67818345876a464d899a400dacecc84
Darya <daryabobur@gmail.com>
2023-03-02 21:00:09 +0200

    Update authOperations.js

--- myNewProject/redux/auth/authOperations.js
+++ myNewProject/redux/auth/authOperations.js
@@ -1,16 +1,38 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-export const authSignIn = () => async(dispatch, getState) => {};
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
-export const authSignUp = ({login, email, password, }) => async(dispatch, getState) => {
+export const authSignIn =
+  ({ email, password }) =>
+  async () => {
     const auth = getAuth();
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      console.log(user);
+    } catch (error) {
+      console.log(error.code);
+      console.log(error.message);
+    }
+  };
 
-try {
-    const user = await createUserWithEmailAndPassword(auth, email, password);
-    console.log(user)
-} catch (error) {
-    console.log(error.code)
-    console.log(error.message)   
-}
-};
+export const authSignUp =
+  ({ login, email, password }) =>
+  async (dispatch, getState) => {
+    const auth = getAuth();
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+        login
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.code);
+      console.log(error.message);
+    }
+  };
 
-export const authSignOut = () => async(dispatch, getState) => {};
+export const authSignOut = () => async (dispatch, getState) => {};
