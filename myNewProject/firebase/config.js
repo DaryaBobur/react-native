bbb145039b422af8366185a397f3c7edd128cb45
Darya <daryabobur@gmail.com>
2023-03-02 21:12:33 +0200

    Update config.js

--- myNewProject/firebase/config.js
+++ myNewProject/firebase/config.js
@@ -1,5 +1,5 @@
-import * as firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAKVI4qhdg8iKtoD6cub1bGZzhCrUfJQKA",
@@ -8,12 +8,8 @@ const firebaseConfig = {
   storageBucket: "socialphoto-cc3ca.appspot.com",
   messagingSenderId: "914061727157",
   appId: "1:914061727157:web:54139d73078465f19bb5fc",
-  measurementId: "G-026MGQWYX2"
+  measurementId: "G-026MGQWYX2",
 };
 
-firebase.initializeApp(firebaseConfig);
-export default firebase;
-
-// const auth = firebase.auth();
-
-// export { auth };
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
