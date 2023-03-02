55ed8184eed5849dd4529546da8f9ee7ee88b9be
Darya <daryabobur@gmail.com>
2023-03-02 21:20:41 +0200

    Update App.js

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -3,8 +3,11 @@ import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config";
 import appRoute from "./router";
 import { store } from "./redux/store";
+
 const loadFonts = async () => {
   await Font.loadAsync({
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
@@ -15,7 +18,12 @@ const loadFonts = async () => {
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const routing = appRoute(false);
+  const [user, setUser] = useState(null);
+
+  onAuthStateChanged(auth, (user) => setUser(user));
+
+  const routing = appRoute(user);
+
   if (!isReady) {
     return (
       <AppLoading
