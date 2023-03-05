76fc1e3a8ef95f27b331a01f13963cb8a1c8dd20
Darya <daryabobur@gmail.com>
2023-03-05 19:59:55 +0200

    Update authReducer.js

--- myNewProject/redux/auth/authReducer.js
+++ myNewProject/redux/auth/authReducer.js
@@ -1,12 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState: {
-        userId: null,
-        email: null
-    },
-    reducers: {
-
-    }
-})
+  name: "auth",
+  initialState: {
+    userId: null,
+    email: null,
+  },
+  reducers: {
+    updateUserProfile: (state, payload) => ({ ...state, email: payload.email }),
+  },
+});
