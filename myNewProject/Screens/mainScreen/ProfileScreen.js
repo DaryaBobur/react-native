d87faa41955db6f6713a99e12dd73b0138124c94
Darya <daryabobur@gmail.com>
2023-03-14 19:59:44 +0200

    Update ProfileScreen.js

--- myNewProject/Screens/mainScreen/ProfileScreen.js
+++ myNewProject/Screens/mainScreen/ProfileScreen.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { authSignOut } from "../redux/auth/authOperations";
 
 const ProfileScreen = () => {
-    return <View style={styles.container}><Text>ProfileScreen</Text></View>
-}
+  return (
+    <View style={styles.container}>
+      <Text>ProfileScreen</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 export default ProfileScreen;
