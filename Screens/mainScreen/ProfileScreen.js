3f82261fcd8beeb8bb83ec21c3b9a02f9840ce4b
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-05 16:42:46 +0300

    Update ProfileScreen.js

--- Screens/mainScreen/ProfileScreen.js
+++ Screens/mainScreen/ProfileScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { authSignOut } from "../../redux/auth/authOperations";
+import { useDispatch } from "react-redux";
 
 const ProfileScreen = () => {
+  const dispatch = useDispatch();
+
+  const signOut = () => {
+    dispatch(authSignOut())
+  }
   return (
     <View>
       <View style={styles.container}>
         <Text>ProfileScreen</Text>
       </View>
-      <TouchableOpacity activeOpacity={0.8}>
+      <TouchableOpacity activeOpacity={0.8} onPress={signOut}>
         <Text>Sign Out</Text>
       </TouchableOpacity>
     </View>
