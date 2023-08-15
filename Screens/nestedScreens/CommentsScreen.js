af3d329fddca3e86e81ea3ac83f88459a367dd18
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-15 12:01:28 +0300

    Update CommentsScreen.js

--- Screens/nestedScreens/CommentsScreen.js
+++ Screens/nestedScreens/CommentsScreen.js
@@ -1,27 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  FlatList,
+  SafeAreaView,
 } from "react-native";
 import { db } from "../../firebase/config";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSelector } from "react-redux";
 
 const CommentsScreen = ({ route }) => {
   const [comment, setComment] = useState("");
+  const [allComments, setAllComments] = useState([]);
   const { postId } = route.params;
   const { login } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    getAllComments();
+  }, []);
 
   const createComment = async () => {
     const newCollection = collection(db, "posts", postId, "comments");
     await addDoc(newCollection, { comment, login });
   };
 
+  const getAllComments = async () => {
+    const newCollection = collection(db, "posts", postId, "comments");
+    await onSnapshot(newCollection, (data) =>
+      setAllComments(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    );
+  };
+
   return (
     <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={allComments}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.login}</Text>
+              <Text>{item.comment}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
       <View>
         <TextInput
           style={styles.input}
